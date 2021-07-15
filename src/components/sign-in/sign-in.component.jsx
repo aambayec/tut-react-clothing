9f06f66e0df7046c8ded35e95b1bf92df6941c6e
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-16 05:06:20 +0800

    fix required validation onclick of sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -58,7 +58,11 @@ class SignIn extends Component {
 
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign In with Google
             </CustomButton>
           </div>
