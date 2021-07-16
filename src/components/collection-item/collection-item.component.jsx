655c3ff80ff3b3367b6523576127634529815e4b
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-16 08:08:42 +0800

    collection item style and add button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,3 +1,4 @@
+import CustomButton from "../custom-button/custom-button.component";
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -12,6 +13,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton inverted>ADD TO CART</CustomButton>
   </div>
 );
 
