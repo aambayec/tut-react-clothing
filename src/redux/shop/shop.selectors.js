9e25477c0e164346d01a93764610379d1cd4de2c
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-20 23:45:42 +0800

    collections overview selector

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -13,3 +13,8 @@ export const selectCollection = memoize((collectionUrlParam) =>
     (collections) => collections[collectionUrlParam]
   )
 );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
