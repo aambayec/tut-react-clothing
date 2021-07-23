076e3a91a9d24bfe06086c68c5aacb4954b19a7f
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-24 03:37:38 +0800

    collections useContext

--- src/context/collections/collections.context.js
+++ src/context/collections/collections.context.js
@@ -1,4 +1,4 @@
-import { createContext } from "jest-runtime";
+import { createContext } from "react";
 import SHOP_DATA from "./shop.data";
 
 const CollectionsContext = createContext(SHOP_DATA);
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,28 +1,22 @@
-import { connect } from "react-redux";
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import { useContext } from "react";
 import CollectionsContext from "../../context/collections/collections.context";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 import "./collection.styles.scss";
 
 const CollectionPage = ({ match }) => {
-  return (
-    <CollectionsContext.Consumer>
-      {(collections) => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
 
-        return (
-          <div className="collection-page">
-            <h2 className="title">{title}</h2>
-            <div className="items">
-              {items.map((item) => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        );
-      }}
-    </CollectionsContext.Consumer>
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
 
