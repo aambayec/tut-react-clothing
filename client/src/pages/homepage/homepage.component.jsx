c3694c950838443b3ae631ee5b75ce0c1f49bbaf
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-25 22:35:37 +0800

    sample use for react profiler

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,17 @@
+import { Profiler } from "react";
 import Directory from "../../components/directory/directory.component";
 import { HomePageContainer } from "./homepage.styles";
 
 const HomePage = () => (
   <HomePageContainer>
-    <Directory />
+    <Profiler
+      id="Directory"
+      onRender={(id, phase, actualDuration) =>
+        console.log({ id, phase, actualDuration })
+      }
+    >
+      <Directory />
+    </Profiler>
   </HomePageContainer>
 );
 
